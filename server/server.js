10f4dbb34e15868aa4f514bcf0e45d1b084e070e
Gokul Kumarresen <gokulk@live.com>
2019-02-16 20:52:24 -0500

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -7,16 +7,20 @@ const User = require("./models/user").User;
 
 const app = express(); 
 
-app.use((req, res, next) => {
-    let data = []
-    req.on("data", (chunk) => {
-        data.push(chunk);
-    }).on("end", () => {
-        data = Buffer.concat(data).toString();
-        req.body = JSON.parse(data); 
-        next(); 
-    });
-}); 
+// app.use((req, res, next) => {
+//     // console.log(req); 
+//     let data = []
+//     req.on("data", (chunk) => {
+
+//         data.push(chunk);
+//     }).on("end", () => {
+//         data = Buffer.concat(data);
+//         req.body = JSON.parse(data); 
+//         next(); 
+//     });
+// }); 
+
+app.use(bodyParser.json()); 
 
 app.post("/todos", (req, res) => {
     let body, todo;
@@ -30,6 +34,16 @@ app.post("/todos", (req, res) => {
         res.send(doc); 
     }, (e) => {
         res.status(400).send(e); 
+    }); 
+});
+
+app.get("/todos", (req, res) => {
+    Todo.find().then((data) => {
+        res.send({
+            todos: data
+        }); 
+    }, (e) => {
+        res.status(400).send(e);
     }); 
 });
 
